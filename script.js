d77340c057707dcc8efcbd066ff3a9e494bd6402
Dmitry Groshkov <bygeorg2623@gmail.com>
2024-05-02 16:35:34 +0300

    Первое домашнее задание

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const a = 1
-let b = "вторая переменная"
+let title = "Первая переменная";
+let screens = "Вторая переменная";
+let screenPrice = "Третья переменная";
+let rollback = "Четвертая переменная";
+let fullPrice = "Пятая переменная";
+let adaptive = "Шестая переменная";
+
+alert('Привет, преподаватель!')
+console.log("Как дела?");
